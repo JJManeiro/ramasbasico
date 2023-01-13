692ad04b279404e0a59976beb8737d1edb548248
Juan José Maneiro <jmaneirogarcia@danielcastelao.org>
2023-01-13 08:40:46 +0100

    E

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //commit A
         //commit B
+        //commit C
+        //commit D
+        //commit E
         System.out.println("Hello world!");
     }
 }
